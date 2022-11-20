49e5939bc04027155e0c7440e3241708220da545
etali-m <etalijunior15@gmail.com>
2022-11-20 11:29:49 +0100

    data model

--- store/models.py
+++ store/models.py
@@ -10,11 +10,15 @@ class Customer(models.Model):
         return self.name
 
 
+"""
+    La classe Product défini un produit avec toutes ses caratéristiques
+"""
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.IntegerField()
     image = models.ImageField(null=True, blank = True)
-    stock = models.IntegerField()
+    stock = models.BooleanField(default=True, null=True, blank=False)
 
     def __str__(self):
         return self.name
@@ -27,6 +31,71 @@ class Product(models.Model):
             url = ''
         return url
 
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=30)
+    origine = models.CharField(max_length=30)
+    #logo = models.ImageField()
+
+
+class Moto(Product):
+    brand = models.OneToOneField(Brand, on_delete=models.CASCADE)
+    chassis = models.IntegerField()
+    cylindre = models.FloatField()
+    nbre_vitesse = models.IntegerField(blank=True)
+    vignette = models.FloatField()
+    type_model = models.CharField(max_length=20)
+    poids = models.DecimalField(max_digits = 7, decimal_places = 2)
+    tension_batterie = models.DecimalField(max_digits = 7, decimal_places=2)
+    puissance_admin = models.FloatField()
+    capacite_recervoir = models.DecimalField(max_digits = 5, decimal_places = 2)
+    nbre_place = models.IntegerField()
+    coulour = models.CharField(max_length=20)
+    garantie = models.DateField()
+
+
+
+
+    class Meta:
+        abstract = True
+
+
+class Accessory(Product):
+    color = models.CharField(max_length=20, blank=True, null=True)
+    description = models.TextField()
+
+
+"""
+    La classe Tricycle permet de définir la structure d'un tricycle
+"""
+class Tricycle(Moto):
+    wheels_number = models.IntegerField(default=3)
+
+
+"""
+    La class MotorBike permet de définir la structure d'une motocyclette
+"""
+class MotorBike(Moto):
+    VILLE = 'VILLE'
+    COMMERCIAL = 'COMMERCIAL'
+    AVENTURE = 'AVENTURE'
+
+    USAGE_CHOICES = (
+        (VILLE, 'ville'),
+        (COMMERCIAL, 'commercial'),
+        (AVENTURE, 'aventure')
+    )
+
+    motor_warranty = models.DateField()
+    usage = models.CharField(max_length=30, choices=USAGE_CHOICES)
+
+
+
+
+"""
+    La classe Order permet de définir une commande
+"""
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     date_order = models.DateTimeField(auto_now_add =True)
@@ -36,3 +105,31 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+"""
+    cette classe permet de définir une ligne de commande
+    une commande peut avoir plusieur ligne de commande et une 
+    ligne de commande concerne un produit
+"""
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+""" 
+    La classe ShippingAddress contient toute les informations sur 
+    l'adresse de livraison d'une commande.
+"""
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    quater = models.CharField(max_length=200, null=False)
+    codepostal = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
